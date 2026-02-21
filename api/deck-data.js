b79f1ed7fb6ba667f36de1d24cc859e0e890bf79
franslukahandika <franslukahandika@gmail.com>
2026-02-21 13:52:49 +0700

    remove better-sqlite3 for vercel

--- api/deck-data.js
+++ api/deck-data.js
@@ -1,12 +1,3 @@
-index-Bwd39hrw.js:557  GET https://kopsimdeck.vercel.app/api/deck-data 500 (Internal Server Error)
-(anonymous) @ index-Bwd39hrw.js:557
-Rs @ index-Bwd39hrw.js:48
-Hl @ index-Bwd39hrw.js:48
-Zs @ index-Bwd39hrw.js:48
-Hl @ index-Bwd39hrw.js:48
-Zs @ index-Bwd39hrw.js:48
-Hl @ index-Bwd39hrw.js:48
-om @ index-Bwd39hrw.js:48
-(anonymous) @ index-Bwd39hrw.js:48
-xe @ index-Bwd39hrw.js:25Understand this error
-index-Bwd39hrw.js:557 SyntaxError: Unexpected token 'A', "A server e"... is not valid JSON
+module.exports = async function (req, res) {
+  res.status(200).json({ status: "ok" });
+};
